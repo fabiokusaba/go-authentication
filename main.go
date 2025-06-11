1d53fa63221585d35d02e195caf0131124bd8285
Fabio Kusaba <fabinhokb44@gmail.com>
2025-06-10 22:02:20 -0300

    feat: add application entry point

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"authentication/config"
+	"authentication/helpers"
+	"authentication/routes"
+	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Starting application...")
+
+	key := config.GenerateRandomKey()
+	helpers.SetJWTKey(key)
+	fmt.Printf("Generated key: %s\n", key)
+
+	r := gin.Default()
+
+	routes.SetupRoutes(r)
+
+	r.Run(":8080")
+	log.Println("Server is running on port: 8080")
 }
